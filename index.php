bb4d59a6d6b0876161bda23e463e2fc6924df8bf
JhervisArevalo <jrareval110104@gmail.com>
2025-07-31 02:59:16 +0800

    fresh start

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
-include_once 'config/database.php';
-
 session_start();
 
-if (isset($_SESSION['user_id'])) 
-{
-    $user_id = $_SESSION['user_id'];
+if (isset($_SESSION['user_id'])) {
+    header("Location: dashboard/index.php");
+} else {
+    header("Location: auth/login.php");
 }
-else
-{
-    $user_id = '';
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    
-</body>
-</html>
+exit();
